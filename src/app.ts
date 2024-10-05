2f2d36401f7814f41b5d849dd776269c29c31c90
raianshahrear <raian@nextgenitltd.com>
2024-10-05 19:43:16 +0600

    globalError and notFound added

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import router from './app/routes';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
 const app: Application = express();
 
 // parser
 app.use(express.json());
-app.use(cors({origin: true, credentials: true}));
+app.use(cors({ origin: true, credentials: true }));
 app.use(cookieParser());
 
 // application route
@@ -17,7 +19,7 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 // application middleware
-// app.use(globalErrorHandler);
-// app.use(notFound);
+app.use(globalErrorHandler);
+app.use(notFound);
 
 export default app;
